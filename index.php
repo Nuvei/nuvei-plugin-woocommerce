c3365ebfbce1cfe685f135b6ff4266acd65dc778
miroslavs <miroslavs@srv-bsf-trm04.gw-4u.com>
2022-11-23 18:43:36 +0200

    Tested on WP 6.1.1 and WC 7.1.0.

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
  * Text Domain: nuvei_checkout_woocommerce
  * Domain Path: /languages
  * Require at least: 4.7
- * Tested up to: 6.1
+ * Tested up to: 6.1.1
  * WC requires at least: 3.0
- * WC tested up to: 7.0.1
+ * WC tested up to: 7.1.0
 */
 
 defined('ABSPATH') || die('die');
